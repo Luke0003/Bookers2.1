ad6c5e237e5af02f47ed566b3185f696532da867
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-16 14:59:28 +0000

    [Update]SignUp機能の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :config_permit_parameters, if: :devise_controller?
+
+  protected
+
+  def config_permit_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
 end
+
