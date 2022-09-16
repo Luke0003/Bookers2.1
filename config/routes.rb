a343901c7b61177f2f5d85e9dd2f3e9ca3410c57
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-16 06:02:17 +0000

    [Fix]ルーティングエラーの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "homes/#top"
+  root "homes#top"
   get "homes/about" => "homes#about", as: "about"
   devise_for :users
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
