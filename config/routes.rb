471193f84c865c7c9e7323af3de9595c782a44e3
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-16 05:45:51 +0000

    [Add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/creath'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'homes/top'
-  get 'homes/about'
+  root "homes/#top"
+  get "homes/about" => "homes#about", as: "about"
   devise_for :users
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
