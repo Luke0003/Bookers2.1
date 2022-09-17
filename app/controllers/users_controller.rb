cccb0c857897f8ff3304c7c289fcfd01fb709f00
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-17 02:34:27 +0000

    [Add]UserInfo & Newbookの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
+  end
+
+  def edit
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "homes#top"
   get "homes/about" => "homes#about", as: "about"
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 
 
