e85a06a889fdd75914b06a2574ef4cbbaab2a0e1
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-20 12:43:57 +0000

    [Add]コメント機能の実装(完成)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
+    @book_comment = BookComment.new
   end
 
   def create
